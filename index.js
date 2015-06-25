df15c1fa98743176fd5697e024dfea10f455ef28
Aria Stewart <aredridel@dinhe.net>
2015-06-25 12:49:11 -0400

    More refactoring discretely

--- index.js
+++ index.js
@@ -6,31 +6,19 @@ var path = require('path');
 var VError = require('verror');
 var semver = require('semver');
 var bluebird = require('bluebird');
-var Promise = bluebird.Promise;
 var fs = bluebird.promisifyAll(require('fs'));
 var glob = bluebird.promisify(require('glob'));
 var strip = require('strip-json-comments');
 
 module.exports = function checkUpgrade(dir, cb) {
-    var options = {
-        protocols: {},
-        onconfig: function () {},
-        basedir: dir,
-        mountpath: null,
-        inheritViews: false
-    };
 
-    fs.readFileAsync(path.resolve(options.basedir, 'package.json'), 'utf-8').then(function (pkg) {
+    fs.readFileAsync(path.resolve(dir, 'package.json'), 'utf-8').then(function (pkg) {
         var info = JSON.parse(pkg);
         if (!semver.satisfies('1.0.5', info.dependencies['kraken-js'])) {
             return cb(new VError("package '%s' does not depend on kraken-js 1.0 and cannot be upgraded", info.name));
         }
-    }).then(function () {
-        return getConfig(options).then(function (config) {
-            var middleware = toNamed(config.get('middleware'));
-            return warningsAboutUnspecifiedEnables(middleware).concat(listDeprecatedMiddleware(middleware));
-        });
-    }).then(function (messages) {
+    }).then(warnAboutConfigs(dir))
+    .then(function (messages) {
         return warningsAboutImportsThatChangedBehavior(dir).then(function (warnings) {
             return messages.concat(warnings);
         });
@@ -48,7 +36,7 @@ function toNamed(obj) {
 
 function getAllConfigsSeparately(dir) {
     return glob(path.join(dir, 'config/*.json')).then(function (results) {
-        return Promise.all(results.map(fetchConfig));
+        return bluebird.all(results.map(fetchConfig));
     });
 }
 
@@ -101,15 +89,30 @@ function warningsAboutUnspecifiedEnables(middleware) {
 }
 
 function warningsAboutImportsThatChangedBehavior(dir) {
-    return getAllConfigsSeparately(dir).then(function (configs) {
-        return configs.filter(configHasImports).map(function (config) {
-            return util.format("config '%s' has import: handlers. These are now resolved as each configuratio is merged, instead of at the end. Make sure this won't break your application", config.file);
-        });
+    return getAllConfigsSeparately(dir).filter(configHasImports).map(function (config) {
+        return util.format("config '%s' has import: handlers. These are now resolved as each configuratio is merged, instead of at the end. Make sure this won't break your application", config.file);
     });
 }
 
 function success(cb) {
     return function (success) {
         return cb(null, success);
+    };
+}
+
+function warnAboutConfigs(dir) {
+    var options = {
+        protocols: {},
+        onconfig: function () {},
+        basedir: dir,
+        mountpath: null,
+        inheritViews: false
+    };
+
+    return function () {
+        return getConfig(options).then(function (config) {
+            var middleware = toNamed(config.get('middleware'));
+            return warningsAboutUnspecifiedEnables(middleware).concat(listDeprecatedMiddleware(middleware));
+        });
     };
 }
